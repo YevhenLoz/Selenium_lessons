81ae688d730ff9201dd4383114138492082048f0
YevhenLoz <98827082+YevhenLoz@users.noreply.github.com>
2022-11-24 04:52:42 +0200

    Delete hw_13 directory

--- hw_13/utilities/web_ui/locator.py
+++
@@ -1,9 +0,0 @@
-class Locator:
-    def __init__(self, method, locator: str):
-        self.__method = method
-        self.__locator = locator
-
-    def get_locator(self):
-        return self.__method, self.__locator
-
-
